b20cab427a5d2f631250e9e852097aabb670a737
Juan David Franco <jfrancoh5@gmail.com>
2026-03-13 01:14:26 -0500

    feat: implementar tareas REST para la gestión de mascotas utilizando el patrón Screenplay.

---
+++ src/test/java/com/pets/tasks/PetGet.java
@@ -0,0 +1,25 @@
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
+import net.serenitybdd.screenplay.rest.interactions.Get;
+import net.serenitybdd.screenplay.Tasks;
+
+public class PetGet implements Task {
+	private final int petId;
+
+	public PetGet(int petId) {
+		this.petId = petId;
+	}
+
+	public static PetGet withId(int petId) {
+		return Tasks.instrumented(PetGet.class, petId);
+	}
+
+	@Override
+	public <T extends Actor> void performAs(T actor) {
+		String resource = "/pet/" + petId;
+		actor.attemptsTo(
+			Get.resource(resource)
+		);
+	}
+}
