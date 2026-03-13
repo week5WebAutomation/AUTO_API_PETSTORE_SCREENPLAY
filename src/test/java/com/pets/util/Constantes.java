02f9689f7345482917d28f298006aa3b74d3eda4
Juan David Franco <jfrancoh5@gmail.com>
2026-03-13 01:53:04 -0500

    feat: correcciones al documento abrir navegador

---
+++ src/test/java/com/pets/util/Constantes.java
@@ -0,0 +1,6 @@
+package test.java.com.pets.util;
+
+public class Constantes {
+    public static final String URL = "https://petstore.swagger.io/#/";
+    public static final String ACTOR = "Chester";
+}
